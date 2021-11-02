ddc1f130aa28a33c3480d040389fdb7044d1151a
jacquesdubois <jacquesadubois@gmail.com>
2021-11-02 15:22:45 -0700

    Dynamically render logged-in user phot in stories with an 'Add to Story' icon

--- pages/components/Stories.js
+++ pages/components/Stories.js
@@ -1,9 +1,13 @@
 import faker from 'faker';
 import { useState, useEffect } from 'react';
 import Story from './Story';
+import { useSession } from 'next-auth/react';
+import { PlusIcon } from '@heroicons/react/solid';
 
 
 function Stories() {
+    const { data: session } = useSession();
+
     const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
@@ -18,6 +22,17 @@ function Stories() {
 
     return (
         <div className='flex space-x-4 p-6 bg-white mt-8 border border-gray-200 rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-gray-100'>
+            {session && (
+                <div className='relative'>
+                    <Story
+                        key='me'
+                        img={session.user.image}
+                        username={session.user.username}
+                    />
+                    <PlusIcon className='absolute bottom-4 -right-1 text-xs w-5 h-5 bg-blue-500 rounded-full flex items-center justify-center text-white'/>
+                </div>
+            )}
+
             {suggestions.map(profile => (
                 <Story
                     key={profile.id}
