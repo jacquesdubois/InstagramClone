aeaafc617dabe4c89dcabeaf6b13ba1d55495ca6
jacquesdubois <jacquesadubois@gmail.com>
2021-11-02 14:21:05 -0700

    Added sign-in sign-out functionality; added conditional rendering to MiniProfile depending on logged-in session status

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,9 +1,13 @@
 import Image from 'next/image';
 import { SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon } from '@heroicons/react/outline';
 import { HomeIcon } from '@heroicons/react/solid';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 
 function Header() {
+    const { data: session } = useSession();
+    // console.log(session);
+
     return (
         <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
             <div className='flex justify-between bg-white max-w-5xl mx-5 lg:mx-auto'>
@@ -39,18 +43,27 @@ function Header() {
                 <div className='flex items-center justify-end space-x-4'>
                     <HomeIcon className='navBtn'/>
                     <MenuIcon className='h-6 md:hidden cursor-pointer'/>
-                    <div className='relative navBtn'>
-                        <PaperAirplaneIcon className='navBtn rotate-45'/>
-                        <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
-                    </div>
-                    <PlusCircleIcon className='navBtn'/>
-                    <UserGroupIcon className='navBtn'/>
-                    <HeartIcon className='navBtn'/>
 
-                    <img className='h-10 rounded-full cursor-pointer p-1 border navBtn'
-                        src="https://links.papareact.com/3ke"
-                        alt="profile pic"
-                    />
+                    {session ? (
+                            <>
+                                <div className='relative navBtn'>
+                                    <PaperAirplaneIcon className='navBtn rotate-45'/>
+                                    <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
+                                </div>
+                                <PlusCircleIcon className='navBtn'/>
+                                <UserGroupIcon className='navBtn'/>
+                                <HeartIcon className='navBtn'/>
+
+                                <img className='h-10 w-10 rounded-full cursor-pointer p-1 border navBtn'
+                                    src={session?.user?.image}
+                                    alt="profile pic"
+                                    onClick={ signOut }
+                                />
+                            </>
+                        ) : (
+                            <button onClick={ signIn }>Sign In</button>
+                        )
+                    }
                 </div>
             </div>
         </div>
