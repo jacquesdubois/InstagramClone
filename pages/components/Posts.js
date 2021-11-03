263c7e2a34f9d7ed3885324225b6b146fd361ae5
jacquesdubois <jacquesadubois@gmail.com>
2021-11-03 13:57:01 -0700

    Refactored posts with useEffect and useState hooks that pull posts from firestore db and dynamically update the feed whenever a user adds a new pic

--- pages/components/Posts.js
+++ pages/components/Posts.js
@@ -1,3 +1,6 @@
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../../firebase';
 import Post from './Post';
 
 const posts =[
@@ -25,16 +28,28 @@ const posts =[
 ]
 
 function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(
+        () => onSnapshot(
+            query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+            (snapshot) => {
+                setPosts(snapshot.docs);
+            }
+        ),
+        [db]
+    );
+
     return (
         <div>
             {posts.map((post) => (
                 <Post
                     key={post.id}
                     id={post.id}
-                    username={post.username}
-                    userImg={post.userImg}
-                    img={post.img}
-                    caption={post.caption}
+                    username={post.data().username}
+                    userImg={post.data().profileImg}
+                    img={post.data().image}
+                    caption={post.data().caption}
                 />
             ))}
         </div>
